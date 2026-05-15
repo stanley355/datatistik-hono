9fc6cd7ef21d157b4570c50d7882719419c91ad2
Stanley Winata <winatastanley355@gmail.com>
2026-05-15 10:58:53 +0700

    feat: More logger

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -29,6 +29,7 @@ export const auth = betterAuth({
     requireEmailVerification: true,
     sendResetPassword: async ({ user, token }) => {
       void mailTransport.sendMail({
+        from: ENV.SMTP_FROM,
         to: user.email,
         subject: "DELIFUNDS - Reset your password",
         text: `Click the link to reset your password: ${ENV.frontendUrl}/auth/reset-password?token=${token}`,
@@ -38,12 +39,17 @@ export const auth = betterAuth({
   emailVerification: {
     sendOnSignUp: true,
     sendVerificationEmail: async ({ user, url, token }) => {
-      console.log("[auth] Send Verification Email to ", user.email);
-      void mailTransport.sendMail({
-        to: user.email,
-        subject: "DELIFUNDS - Verify your email address",
-        text: `Click the link to verify your email: ${ENV.frontendUrl}/auth/verify-email?token=${token}`,
-      });
+      try {
+        console.log("[auth] Send Verification Email to ", user.email);
+        void mailTransport.sendMail({
+          from: ENV.SMTP_FROM,
+          to: user.email,
+          subject: "DELIFUNDS - Verify your email address",
+          text: `Click the link to verify your email: ${ENV.frontendUrl}/auth/verify-email?token=${token}`,
+        });
+      } catch (err) {
+        console.error("[auth] Send Verification Email fail", err);
+      }
     },
   },
   advanced: {
